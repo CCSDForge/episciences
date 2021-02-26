105b761f16adfc8fa2a4073aa104b31e0933ecc0
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2021-02-26 12:15:16 +0100

    Add loader.

---
+++ public/js/stats/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    let $loadingStats = $("#loading-stats");
+    $loadingStats.html(getLoader());
+    $loadingStats.show();
+    $(window).load(function () {
+        $loadingStats.hide();
+    });
+});
