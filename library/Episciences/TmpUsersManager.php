605010cdbed6348f3431b9cda424d14a814f4ef3
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2021-03-24 23:58:31 +0100

    Comment + cast ID as int

--- library/Episciences/TmpUsersManager.php
+++ library/Episciences/TmpUsersManager.php
@@ -2,15 +2,19 @@
 
 class Episciences_TmpUsersManager 
 {
+
+    /**
+     * @param int $id
+     * @return Episciences_User_Tmp|false
+     */
 	public static function findById($id)
 	{
 		$db = Zend_Db_Table_Abstract::getDefaultAdapter();
-		$sql = $db->select()->from(T_TMP_USER)->where('ID = ?', $id);
+		$sql = $db->select()->from(T_TMP_USER)->where('ID = ?', (int) $id);
 		$result = $db->fetchRow($sql);
 		
 		if ($result) {
-			$user = new Episciences_User_Tmp($result);
-			return $user;
+            return new Episciences_User_Tmp($result);
 		}
 		
 		return false;
