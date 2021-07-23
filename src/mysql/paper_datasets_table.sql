9536853c5a6f3c5a0cdcba50821db944dff2b344
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2021-07-23 09:19:34 +0200

    Create 'paper_datasets' table: - Switch to encoding 'utf8mb4_unicode_ci' - Fix MySQL Error #1071 - Specified key was too long; max key length is 767 bytes

--- src/mysql/paper_datasets_table.sql
+++ src/mysql/paper_datasets_table.sql
@@ -1,24 +1,29 @@
 --
 -- Table structure
 --
-CREATE TABLE `paper_datasets` (
-                                  `id` int NOT NULL,
-                                  `doc_id` int NOT NULL,
-                                  `code` varchar(50) NOT NULL,
-                                  `name` varchar(200) NOT NULL,
-                                  `value` varchar(500) NOT NULL,
-                                  `link` varchar(750) NOT NULL,
-                                  `source_id` int NOT NULL,
-                                  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+CREATE TABLE `paper_datasets`(
+                                 `id` INT(11) NOT NULL,
+                                 `doc_id` INT(11) NOT NULL,
+                                 `code` VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                 `name` VARCHAR(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                 `value` VARCHAR(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                 `link` VARCHAR(750) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                 `source_id` INT(11) NOT NULL,
+                                 `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 --
 -- Index
 --
-ALTER TABLE `paper_datasets`
-    ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `unique` (`doc_id`,`code`,`name`,`value`,`source_id`) USING BTREE,
-  ADD KEY `doc_id` (`doc_id`),
-  ADD KEY `source_id` (`source_id`),
-  ADD KEY `code` (`code`),
-  ADD KEY `name` (`name`) USING BTREE;
+ALTER TABLE
+    `paper_datasets` ADD PRIMARY KEY(`id`),
+    ADD UNIQUE KEY `unique`(
+        `doc_id`,
+        `code`(15),
+        `name`(35),
+        `value`(47),
+        `source_id`
+    ),
+    ADD KEY `doc_id`(`doc_id`),
+    ADD KEY `source_id`(`source_id`),
+    ADD KEY `code`(`code`(15)),
+    ADD KEY `name`(`name`(35));
