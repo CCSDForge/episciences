14953d61fb0ed75469217a112d3e0115c653c7b3
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2022-02-01 23:29:54 +0100

    add header 'Link' for coar notifiy + signposting protocol
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- application/modules/journal/controllers/BrowseController.php
+++ application/modules/journal/controllers/BrowseController.php
@@ -11,6 +11,19 @@ class BrowseController extends Zend_Controller_Action
      */
     public function init()
     {
+        $headerLinks[] = sprintf('Link: <%s>; rel="http://www.w3.org/ns/ldp#inbox"', INBOX_URL);
+        header(implode(', ', $headerLinks));
+
+        if ($this->getFrontController()->getRequest()->getHeader('Accept') === 'application/ld+json') {
+            $this->_helper->layout()->disableLayout();
+            $this->_helper->viewRenderer->setNoRender();
+            $ldJson['@context'] = "http://www.w3.org/ns/ldp";
+            $ldJson['inbox'] = INBOX_URL;
+            echo json_encode($ldJson);
+            exit;
+        }
+
+
         $solrConfigFile = APPLICATION_PATH . '/../data/' . RVCODE . '/config/solr.json';
 
         if (is_file($solrConfigFile)) {
--- application/modules/journal/controllers/IndexController.php
+++ application/modules/journal/controllers/IndexController.php
@@ -8,6 +8,17 @@ class IndexController extends Zend_Controller_Action
      */
     public function indexAction()
     {
+        $headerLinks[] = sprintf('Link: <%s>; rel="http://www.w3.org/ns/ldp#inbox"', INBOX_URL);
+        header(implode(', ', $headerLinks));
+
+        if ($this->getFrontController()->getRequest()->getHeader('Accept') === 'application/ld+json') {
+            $this->_helper->layout()->disableLayout();
+            $this->_helper->viewRenderer->setNoRender();
+            $ldJson['@context'] = "http://www.w3.org/ns/ldp";
+            $ldJson['inbox'] = INBOX_URL;
+            echo json_encode($ldJson);
+            exit;
+        }
         $this->view->controller = 'index';
         $this->forward('index', 'page');
     }
