601ae554864cc3d5f7fd7c1a1b1f234f9de3cb0c
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2022-05-09 18:12:45 +0200

    "translate" license URLs
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

---
+++ application/languages/fr/license.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'https://arxiv.org/licenses/assumed-1991-2003' => 'arXiv.org - Assumed non-exclusive license to distribute',
+    'https://arxiv.org/licenses/nonexclusive-distrib/1.0' => 'arXiv.org - Non-exclusive license to distribute',
+    'https://creativecommons.org/licenses/by-nc-nd/4.0' => 'Attribution-Non Commercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)',
+    'https://creativecommons.org/licenses/by-nc-sa/3.0' => 'Attribution-Non Commercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)',
+    'https://creativecommons.org/licenses/by-nc-sa/4.0' => 'Attribution-Non Commercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)',
+    'https://creativecommons.org/licenses/by-sa/4.0' => 'Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)',
+    'https://creativecommons.org/licenses/by/1.0' => 'Attribution 1.0 Generic (CC BY 1.0)',
+    'https://creativecommons.org/licenses/by/3.0' => 'Attribution 3.0 Unported (CC BY 3.0)',
+    'https://creativecommons.org/licenses/by/4.0' => 'Attribution 4.0 International (CC BY 4.0)',
+    'https://creativecommons.org/publicdomain/zero/1.0' => 'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+
+];
+
