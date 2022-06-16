8e7722fb2777894de5c33930903ba5a9d03d4c62
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2022-06-16 15:56:52 +0200

    Fixed: author's affiliations: ROR autocomplete fails when selecting an author: "versionCache" variable is misread.

--- public/js/paper/paperAffiAuthors.js
+++ public/js/paper/paperAffiAuthors.js
@@ -1,31 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
     affiliationManagement();
 });
 
-function affiliationManagement(){
-    $("#select-author-affi").change(function(e){
-        let idAuthor = $(this).find(":selected").attr("id");
-        $('#id-edited-affi-author').val(idAuthor);
-        if (typeof idAuthor !== 'undefined') {
-            $.ajax({
-                url: '/paper/getaffiliationsbyauthor/',
-                type: 'POST',
-                data: JSON.stringify({
-                    'idAuthor' : idAuthor,
-                    'paperId' : $("div#paperid-for-author").text()
-                }),
-                success: function success(result) {
+function affiliationManagement() {
+    $("#select-author-affi").on('change', function () {
+            let idAuthor = $(this).find(":selected").attr("id");
+            $('#id-edited-affi-author').val(idAuthor);
+            if (typeof idAuthor !== 'undefined') {
+
+                let data = JSON.stringify({
+                    'idAuthor': idAuthor,
+                    'paperId': $("div#paperid-for-author").text()
+                });
+
+                let ajaxR = ajaxRequest('/paper/getaffiliationsbyauthor/', data);
+
+                ajaxR.done(function (result) {
+
                     $('div#affi-body').empty();
                     $('div#affi-body').append(result);
-                    $.getScript('/js/user/affiliations.js?_=v'+versionCache).fail(function () {
+
+                    let url = '/js/user/affiliations.js?_=v' + versionCache;
+
+                    $.getScript(url).fail(function () {
                         console.log('affiliations.js loading failed');
                     });
-                }
-            });
-        }else{
-            $('div#affi-body').empty();
+
+                });
+
+            } else {
+                $('div#affi-body').empty();
+            }
+
+
         }
-    });
+    );
     $("form#form-affi-authors").submit(function (e) {
         if (!$('#id-edited-affi-author').val() || $('#id-edited-affi-author').val().length === 0) {
             e.preventDefault();
