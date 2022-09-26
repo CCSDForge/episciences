eed437041f67b1e0afe47395f0d2fd7948addbee
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2022-09-26 14:50:17 +0200

    link to old version: refactoring.

--- application/modules/journal/controllers/PaperController.php
+++ application/modules/journal/controllers/PaperController.php
@@ -1968,7 +1968,8 @@ class PaperController extends PaperDefaultController
             $result['message'] = $translator->translate("Cet article a été refusé, il n'est plus nécessaire de le relire.");
             $result['url'] = $url;
         } elseif ($paper->isObsolete()) { // paper is obsolete: display a notice
-            $paper->getLatestVersionId();
+            $latestDocId = $paper->getLatestVersionId();
+            $this->view->linkToLatestDocId = $this->buildAdminPaperUrl($latestDocId);
             $result['displayNotice'] = true;
         }
 
