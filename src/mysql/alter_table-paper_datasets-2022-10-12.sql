77da50e4ea1aa46043db4c08d5ee94e9694c71b2
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2022-10-14 10:48:47 +0200

    fix typo.

--- src/mysql/alter_table-paper_datasets-2022-10-12.sql
+++ src/mysql/alter_table-paper_datasets-2022-10-12.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `paper_dataset` ADD `relationship` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL AFTER `source_id`, ADD `id_paper_datasets_meta` INT UNSIGNED NULL AFTER `relationship`;
+ALTER TABLE `paper_datasets` ADD `relationship` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL AFTER `source_id`, ADD `id_paper_datasets_meta` INT UNSIGNED NULL AFTER `relationship`;
 ALTER TABLE `paper_datasets` ADD INDEX(`id_paper_datasets_meta`);
 ALTER TABLE `paper_datasets` ADD CONSTRAINT `deleteAssocMeta` FOREIGN KEY (`id_paper_datasets_meta`) REFERENCES `paper_datasets_meta` (`id`) ON DELETE CASCADE;
