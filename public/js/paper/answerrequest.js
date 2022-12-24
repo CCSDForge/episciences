903b38f0e45b548c08cadb23b624ad63e0b6ab9f
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2022-12-24 15:50:04 +0100

    add missing Js file

---
+++ public/js/paper/answerrequest.js
@@ -0,0 +1,31 @@
+__initMCE("textarea");
+
+let $hRequiredPwd = $('#h_requiredPwd');
+let $paperPassword = $('#paperPassword');
+
+let $commentOnly = $('[id^=comment_only_]');
+
+let isEmptyComment = $commentOnly.length > 0 && $commentOnly.val() === '';
+
+let isRequiredPaperPwd = ($hRequiredPwd.length > 0 && $hRequiredPwd.val() === '1');
+let isEmptyPaperPwd = ($paperPassword.length > 0 && $paperPassword.val() === '');
+
+checkCurrentForm();
+
+tinyMCE.activeEditor.on('input', function () {
+    isEmptyComment = ($(this)[0].getContent() === '');
+    checkCurrentForm();
+});
+
+
+$paperPassword.on('input', function () {
+    isEmptyPaperPwd = $paperPassword.val() === '';
+    checkCurrentForm();
+
+});
+
+
+function checkCurrentForm() {
+    let isValidForm = !isEmptyComment && (!isRequiredPaperPwd || (isRequiredPaperPwd && !isEmptyPaperPwd));
+    isValidForm ? enableModalSubmitButton() : disableModalSubmitButton();
+}
