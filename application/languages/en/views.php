449c42f9cd97b7a398c800d719c22828b81d18c1
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2023-05-30 14:05:01 +0200

    UI/UX: make export button shorter
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- application/languages/en/views.php
+++ application/languages/en/views.php
@@ -1805,7 +1805,6 @@ return [
     'Rédacteur sans rubrique' => 'Editor without section',
 
     "Exporter" => "Export",
-    "Export métadonnées" => "Export metadata",
     "Partager" => "Share",
     "Partager et exporter" => 'Share and export',
     "Voulez-vous partager la publication ? Rendez-vous" => "Do you want to share the publication? Go",
