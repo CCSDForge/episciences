ce95f80ce218e78e064279806ce6612c6cb853e2
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2023-07-10 18:01:03 +0200

    refactoring.

--- library/Episciences/Repositories/Zenodo/Hooks.php
+++ library/Episciences/Repositories/Zenodo/Hooks.php
@@ -117,16 +117,7 @@ class Episciences_Repositories_Zenodo_Hooks implements Episciences_Repositories_
      */
     public static function hookIsOpenAccessRight(array $hookParams): array
     {
-        $isOpenAccessRight = false;
-        $pattern = '<dc:rights>info:eu-repo\/semantics\/openAccess<\/dc:rights>';
-
-        if (array_key_exists('record', $hookParams)) {
-            $found = Episciences_Tools::extractPattern('/' . $pattern . '/', $hookParams['record']);
-            $isOpenAccessRight = !empty($found);
-        }
-
-
-        return ['isOpenAccessRight' => $isOpenAccessRight];
+        return Episciences_Repositories_Common::isOpenAccessRight($hookParams);
     }
 
     public static function hookHasDoiInfoRepresentsAllVersions(array $hookParams): array
