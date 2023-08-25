a7dbb853c8403c24ee3a555d12485857538e737b
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2023-08-25 09:49:36 +0200

    Fix incomplete journals sitemaps
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- application/modules/journal/controllers/SitemapController.php
+++ application/modules/journal/controllers/SitemapController.php
@@ -7,28 +7,52 @@ class SitemapController extends Zend_Controller_Action
     {
         $this->_helper->layout()->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
+
+        $sitemapEntries = $this->getSitemapEntries();
+
         header('Content-Type: text/xml; charset: utf-8');
-        echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
-        echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.PHP_EOL;
-        
-        $query  = 'q=*%3A*';
-        $query .= '&sort=publication_date_tdate+desc&wt=phps&omitHeader=true';        
+        echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+        echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+        echo $sitemapEntries;
+        echo '</urlset>' . PHP_EOL;
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    private function getSitemapEntries(): string
+    {
+        $sitemapEntries = '';
+        $query = 'q=*%3A*';
+        $query .= '&sort=publication_date_tdate+desc&wt=phps&omitHeader=true&rows=50000&fl=docid,publication_date_tdate';
+
         if (RVID && RVID != 0) {
-        	$query .= '&fq=revue_id_i:'.RVID;
+            $query .= '&fq=revue_id_i:' . RVID;
         }
+
         $result = Episciences_Tools::solrCurl($query, 'episciences', 'select', true);
         if ($result) {
-        	$result = unserialize($result, ['allowed_classes' => false]);
-        	foreach ($result['response']['docs'] as $paper) {
-        		echo "\t" . "<url>" . PHP_EOL;
-        		echo "\t\t" . "<loc>" . APPLICATION_URL.'/'.$paper['docid'] . "</loc>" . PHP_EOL; 		// URL de l'article
-        		echo "\t\t" . "<lastmod>" . $paper['publication_date_tdate'] . "</lastmod>" . PHP_EOL;	// Date de publication de l'article
-        		echo "\t\t" . "<changefreq>daily</changefreq>" . PHP_EOL;
-        		echo "\t\t" . "<priority>1</priority>" . PHP_EOL;
-        		echo "\t" . "</url>" . PHP_EOL;
-        	}
+            $result = unserialize($result, ['allowed_classes' => false]);
+            if (!empty($result['response']['docs'])) {
+                foreach ($result['response']['docs'] as $paper) {
+                    $sitemapEntries .= $this->createOneSitemapEntry($paper);
+                }
+            }
         }
-        echo '</urlset>'.PHP_EOL;
+        return $sitemapEntries;
+    }
+
+    /**
+     * @param $paper
+     * @return string
+     */
+    private function createOneSitemapEntry($paper): string
+    {
+        $entry = "<url>";
+        $entry .= "<loc>" . APPLICATION_URL . '/' . $paper['docid'] . "</loc>";
+        $entry .= "</url>" . PHP_EOL;
+        return $entry;
     }
 
 }
