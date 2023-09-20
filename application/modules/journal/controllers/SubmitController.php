bbb59846cfc1b33f195dd19b7c5e3eb7fe919010
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2023-09-20 17:50:28 +0200

    fixed Notice: Undefined offset.

--- application/modules/journal/controllers/SubmitController.php
+++ application/modules/journal/controllers/SubmitController.php
@@ -182,7 +182,8 @@ class SubmitController extends DefaultController
                 //remove episciences from repositories list
                 continue;
             }
-            $examples[$id] = Episciences_Repositories::$_identifierExemples[$repository['id']];
+
+            $examples[$id] = Episciences_Repositories::$_identifierExemples[$repository['id']] ?? '';
         }
 
         $allowedRepositories = Episciences_Submit::getRepositoriesLabels($settings);
