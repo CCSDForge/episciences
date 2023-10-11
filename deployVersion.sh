b0a26f5212442f7b27d19434916c33d0031e793d
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2023-10-11 18:32:48 +0200

    default deploy with php7.4 cli
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- deployVersion.sh
+++ deployVersion.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if test -f "/usr/bin/php7.4"; then
+  PHP_BIN="/usr/bin/php7.4"
+else
+  PHP_BIN="/usr/bin/php"
+fi
+
 deployDate=$(date "+%Y-%m-%d %X %z")
 git fetch --all
 git fetch --tags
@@ -8,6 +15,6 @@ echo '<?php' > 'version.php';
 echo "\$gitHash='$gitHashCommit';">>  'version.php';
 echo "\$gitBranch='$1';">>  'version.php';
 echo "\$deployDate='$deployDate';">>  'version.php';
-php composer.phar install -o --no-dev
+$PHP_BIN composer.phar install -o --no-dev
 yarn encore production
 
