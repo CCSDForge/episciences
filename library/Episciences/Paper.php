0b2d523e8ea322bb900fc0877e39333bb90834df
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2023-10-30 11:44:40 +0100

    fixed: RT#196365: bug with feature "Re-invite this reviewer".

--- library/Episciences/Paper.php
+++ library/Episciences/Paper.php
@@ -1285,7 +1285,14 @@ class Episciences_Paper
      */
     public function canBeReviewed(): bool
     {
-        return ($this->isEditable() && !$this->isAccepted() && !$this->isRevisionRequested());
+        return
+            (
+                $this->isEditable() &&
+                !$this->isAccepted() &&
+                !$this->isRevisionRequested() &&
+                !$this->isCopyEditingProcessStarted()&&
+                !$this->isReadyToPublish()
+            );
     }
 
     /**
