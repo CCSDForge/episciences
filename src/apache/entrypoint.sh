5928cd2442b01d02a8f423901c2b0f55c9145f0c
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-09-02 09:09:54 +0200

    updated docker config
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- src/apache/entrypoint.sh
+++ src/apache/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Optionally, include the vhost in the main configuration
+echo "Include conf/extra/episciences.conf" >> /usr/local/apache2/conf/httpd.conf
 #chown -R www-data:www-data /var/www/data /var/www/cache /var/www/logs
-#exec "$@"
+exec "$@"
 
