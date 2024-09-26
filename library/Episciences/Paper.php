032efd3be079c8a0fcce5dbbd0fe0891840aa82d
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-09-26 08:09:15 +0200

    fixed translation
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- library/Episciences/Paper.php
+++ library/Episciences/Paper.php
@@ -299,7 +299,7 @@ class Episciences_Paper
         self::STATUS_SUBMITTED => 'soumis',
         self::STATUS_OK_FOR_REVIEWING => 'en attente de relecture',
         self::STATUS_BEING_REVIEWED => 'en cours de relecture',
-        self::STATUS_REVIEWED => 'relu en attente de décision éditoriale',
+        self::STATUS_REVIEWED => 'évalué - en attente de décision éditoriale',
         self::STATUS_ACCEPTED => 'accepté',
         self::STATUS_PUBLISHED => 'publié',
         self::STATUS_REFUSED => 'refusé',
