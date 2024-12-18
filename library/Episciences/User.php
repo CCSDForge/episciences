e568eb62205f9799ca144700a43567f9a190d3d9
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-12-18 14:33:26 +0100

    Fix to prevent having empty roles when the last user role is deleted
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- library/Episciences/User.php
+++ library/Episciences/User.php
@@ -1043,6 +1043,11 @@ class Episciences_User extends Ccsd_User_Models_User
                 $values[] = '(' . $uid . ',' . $rvId . ',' . $roleId . ')';
             }
 
+            if (empty($values)) {
+                $roleId = Episciences_Acl::ROLE_MEMBER;
+                $values[] = '(' . $uid . ',' . $rvId . ',' . $roleId . ')';
+            }
+
             // Enregistrement des nouveaux rôles
             $sql = 'INSERT IGNORE INTO ' . T_USER_ROLES . ' (UID, RVID, ROLEID) VALUES ' . implode(',', $values);
             $this->_db->query($sql);
