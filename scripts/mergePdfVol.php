25bf20eab4852ce3c79541bb23cf4cf7f7536e5b
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-01-24 08:34:37 +0100

    reformat code
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- scripts/mergePdfVol.php
+++ scripts/mergePdfVol.php
@@ -22,16 +22,16 @@ class mergePdfVol extends JournalScript
 {
 
     /**
+     * getDoi constructor.
+     * @param $localopts
+     */
+    public const APICALLVOL = "volumes?page=1&itemsPerPage=1000&rvcode=";
+    public const APICALLFORDOCID = "papers?page=1&itemsPerPage=1000&vid=";
+    /**
      * @var bool
      */
     protected bool $_dryRun = true;
 
-    /**
-     * getDoi constructor.
-     * @param $localopts
-     */
-    public CONST APICALLVOL = "volumes?page=1&itemsPerPage=1000&rvcode=";
-    public CONST APICALLFORDOCID = "papers?page=1&itemsPerPage=1000&vid=";
     public function __construct($localopts)
     {
         $this->setRequiredParams([]);
@@ -57,29 +57,29 @@ class mergePdfVol extends JournalScript
             die('ERROR: MISSING RVCODE' . PHP_EOL);
         }
 
-        if ($this->getParam('removecache') === '1'){
-            $cache = new FilesystemAdapter("volume-pdf-".$rvCode, 0 ,dirname(APPLICATION_PATH) . '/cache/');
+        if ($this->getParam('removecache') === '1') {
+            $cache = new FilesystemAdapter("volume-pdf-" . $rvCode, 0, dirname(APPLICATION_PATH) . '/cache/');
             $cache->clear();
         }
         $client = new Client();
-        $response = $client->get(EPISCIENCES_API_URL.self::APICALLVOL.$rvCode)->getBody()->getContents();
-        foreach (json_decode($response, true, 512, JSON_THROW_ON_ERROR)['hydra:member'] as $res){
+        $response = $client->get(EPISCIENCES_API_URL . self::APICALLVOL . $rvCode)->getBody()->getContents();
+        foreach (json_decode($response, true, 512, JSON_THROW_ON_ERROR)['hydra:member'] as $res) {
             $strPdf = '';
-            $getDocIdBypaperApi = $client->get(EPISCIENCES_API_URL.self::APICALLFORDOCID.$res['vid'])->getBody()->getContents();
+            $getDocIdBypaperApi = $client->get(EPISCIENCES_API_URL . self::APICALLFORDOCID . $res['vid'])->getBody()->getContents();
             $docIds = $this->getDocIds($getDocIdBypaperApi, $res['vid']);
             $docIdList = [];
             foreach ($res['papers'] as $paper) {
-                $docIdList[] =  $docIds[$paper['paperid']];
+                $docIdList[] = $docIds[$paper['paperid']];
             }
-            if ($this->getParam('ignorecache') === '1' || (string)(json_decode(self::getCacheDocIdsList($res['vid'],$rvCode), true, 512, JSON_THROW_ON_ERROR) !== $docIdList)) {
+            if ($this->getParam('ignorecache') === '1' || (string)(json_decode(self::getCacheDocIdsList($res['vid'], $rvCode), true, 512, JSON_THROW_ON_ERROR) !== $docIdList)) {
                 foreach ($res['papers'] as $paper) {
                     $docId = $docIds[$paper['paperid']];
-                    $this->displayInfo('docId '. $docId."\n", true);
+                    $this->displayInfo('docId ' . $docId . "\n", true);
                     list($pdf, $pathDocId, $path) = $this->getPdfAndPath($rvCode, $docId);
                     if (!is_dir($path) && !mkdir($path, 0777, true) && !is_dir($path)) {
                         throw new \RuntimeException(sprintf('Directory "%s" was not created', $path));
                     }
-                    $pathPdf = $path.$docId.".pdf";
+                    $pathPdf = $path . $docId . ".pdf";
                     if (!file_exists($pathPdf)) {
                         try {
                             $this->downloadPdf($client, $pdf, $pathPdf, $path, $pathDocId);
@@ -91,57 +91,20 @@ class mergePdfVol extends JournalScript
                         $strPdf .= $pathPdf . ' ';
                     }
                 }
-                $pathPdfMerged = APPLICATION_PATH.'/../data/'.$rvCode.'/public/volume-pdf/'.$res['vid'].'/';
+                $pathPdfMerged = APPLICATION_PATH . '/../data/' . $rvCode . '/public/volume-pdf/' . $res['vid'] . '/';
                 if (!is_dir($pathPdfMerged) && !mkdir($pathPdfMerged, 0777, true) && !is_dir($pathPdfMerged)) {
                     throw new \RuntimeException(sprintf('Directory "%s" was not created', $pathPdfMerged));
                 }
-                self::setCacheDocIdsList((string)$res['vid'],$docIdList,$rvCode);
-                $exportPdfPath = $pathPdfMerged.$res['vid'].'.pdf';
-                $this->displayInfo('merge volume : '. $res['vid']."\n", true);
-                system("pdfunite ".escapeshellcmd($strPdf)." ".escapeshellcmd($exportPdfPath));
+                self::setCacheDocIdsList((string)$res['vid'], $docIdList, $rvCode);
+                $exportPdfPath = $pathPdfMerged . $res['vid'] . '.pdf';
+                $this->displayInfo('merge volume : ' . $res['vid'] . "\n", true);
+                system("pdfunite " . escapeshellcmd($strPdf) . " " . escapeshellcmd($exportPdfPath));
             } else {
                 $this->displayInfo('docids are the same from the API', true);
             }
         }
         $this->displayInfo('Volumes pdf fusion completed. Good Bye ! =)', true);
 
-    }
-
-    /**
-     * @return bool
-     */
-    public
-    function isDryRun(): bool
-    {
-        return $this->_dryRun;
-    }
-
-    /**
-     * @param bool $dryRun
-     */
-    public
-    function setDryRun(bool $dryRun)
-    {
-        $this->_dryRun = $dryRun;
-    }
-    public static function isValidPdf(string $filePath): bool
-    {
-        return mime_content_type($filePath) === 'application/pdf';
-    }
-    public static function getCacheDocIdsList(string $vid,string $rvCode) : string
-    {
-        $cache = new FilesystemAdapter("volume-pdf-".$rvCode, 0 ,dirname(APPLICATION_PATH) . '/cache/');
-        $getVidsList = $cache->getItem($vid);
-        if (!$getVidsList->isHit()) {
-            return json_encode([''], JSON_THROW_ON_ERROR);
-        }
-        return $getVidsList->get();
-    }
-    public static function setCacheDocIdsList($vid, array $jsonVidList, string $rvCode) : void {
-        $cache = new FilesystemAdapter("volume-pdf-".$rvCode, 0 ,dirname(APPLICATION_PATH) . '/cache/');
-        $setVidList = $cache->getItem($vid);
-        $setVidList->set(json_encode($jsonVidList, JSON_THROW_ON_ERROR));
-        $cache->save($setVidList);
     }
 
     /**
@@ -160,30 +123,14 @@ class mergePdfVol extends JournalScript
         return $docIds;
     }
 
-    /**
-     * @param string $pathPdf
-     * @param string $path
-     * @param string $pathDocId
-     * @return void
-     */
-    public function removePdfNotValid(string $pathPdf, string $path, string $pathDocId): void
+    public static function getCacheDocIdsList(string $vid, string $rvCode): string
     {
-        if (!self::isValidPdf($pathPdf)) {
-            unlink($pathPdf);
-            rmdir($path);
-            rmdir($pathDocId);
+        $cache = new FilesystemAdapter("volume-pdf-" . $rvCode, 0, dirname(APPLICATION_PATH) . '/cache/');
+        $getVidsList = $cache->getItem($vid);
+        if (!$getVidsList->isHit()) {
+            return json_encode([''], JSON_THROW_ON_ERROR);
         }
-    }
-
-    /**
-     * @param string $pathPdf
-     * @param string $path
-     * @return void
-     */
-    public function removeDirectory(string $pathPdf, string $path): void
-    {
-        unlink($pathPdf);
-        rmdir($path);
+        return $getVidsList->get();
     }
 
     /**
@@ -218,6 +165,64 @@ class mergePdfVol extends JournalScript
         }
         $this->removePdfNotValid($pathPdf, $path, $pathDocId);
     }
+
+    /**
+     * @param string $pathPdf
+     * @param string $path
+     * @return void
+     */
+    public function removeDirectory(string $pathPdf, string $path): void
+    {
+        unlink($pathPdf);
+        rmdir($path);
+    }
+
+    /**
+     * @param string $pathPdf
+     * @param string $path
+     * @param string $pathDocId
+     * @return void
+     */
+    public function removePdfNotValid(string $pathPdf, string $path, string $pathDocId): void
+    {
+        if (!self::isValidPdf($pathPdf)) {
+            unlink($pathPdf);
+            rmdir($path);
+            rmdir($pathDocId);
+        }
+    }
+
+    public static function isValidPdf(string $filePath): bool
+    {
+        return mime_content_type($filePath) === 'application/pdf';
+    }
+
+    public static function setCacheDocIdsList($vid, array $jsonVidList, string $rvCode): void
+    {
+        $cache = new FilesystemAdapter("volume-pdf-" . $rvCode, 0, dirname(APPLICATION_PATH) . '/cache/');
+        $setVidList = $cache->getItem($vid);
+        $setVidList->set(json_encode($jsonVidList, JSON_THROW_ON_ERROR));
+        $cache->save($setVidList);
+    }
+
+    /**
+     * @return bool
+     */
+    public
+    function isDryRun(): bool
+    {
+        return $this->_dryRun;
+    }
+
+    /**
+     * @param bool $dryRun
+     */
+    public
+    function setDryRun(bool $dryRun)
+    {
+        $this->_dryRun = $dryRun;
+    }
 }
+
 $script = new mergePdfVol($localopts);
 $script->run();
