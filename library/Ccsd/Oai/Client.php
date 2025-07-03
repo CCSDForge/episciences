5a5662b9a8d8b3c5f60f30e97387287ee4f31c3a
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-07-03 11:07:39 +0200

    log get records calls
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- library/Ccsd/Oai/Client.php
+++ library/Ccsd/Oai/Client.php
@@ -84,6 +84,7 @@ class Ccsd_Oai_Client {
             $options[CURLOPT_USERAGENT] = $userAgent;
 			$url = $this->baseUrl.'?verb=GetRecord&identifier='.$identifier.'&metadataPrefix='.$format;
 			$curlResponse = (string)Ccsd_Tools::curlSourceGetContents($url, $options);
+            error_log($url);
 
             if(empty($curlResponse)) {
                 throw new Ccsd_Error("No body returned");
