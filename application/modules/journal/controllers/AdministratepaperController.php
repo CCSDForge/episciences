833076dc2749e0fee55a129943d0851e63bdd843
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-08-09 02:37:18 +0200

    - fixed Call to undefined method Episciences_User::setRegistration_date() + setModification_date - fixed error message not used
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- application/modules/journal/controllers/AdministratepaperController.php
+++ application/modules/journal/controllers/AdministratepaperController.php
@@ -4831,18 +4831,16 @@ class AdministratepaperController extends PaperDefaultController
                 $casUserMapper->find($selectedUserId, $user);
                 $user->setScreenName();
                 $user->setIs_valid();
-                $user->setRegistration_date();
-                $user->setModification_date();
+                $user->setRegistrationDate();
+                $user->setModificationDate();
                 $screenName = $user->getScreenName();
 
                 if ($user->save()) {
                     $success = Zend_Registry::get('Zend_Translate')->translate("L'utilisateur <strong>%%RECIPIENT_SCREEN_NAME%%</strong> a bien été ajouté en tant que co-auteur du document");
                     $success = str_replace('%%RECIPIENT_SCREEN_NAME%%', $screenName, $success);
-                    $addcoAuthor = $this->addRoleCoAuthor((int)$request->getPost('docId'), $selectedUserId);
-                    if ($addcoAuthor) {
-                        $message = Zend_Registry::get('Zend_Translate')->translate("Utilisateur ajouté en tant que co-auteur");
-                    } else {
-                        $message = Zend_Registry::get('Zend_Translate')->translate("L'utilisateur est déjà co-auteur de ce document");
+                    $addcoAuthor = $this->addRoleCoAuthor($docId, $selectedUserId);
+                    if (!$addcoAuthor) {
+                        $success .= ' ' . Zend_Registry::get('Zend_Translate')->translate("L'utilisateur est déjà co-auteur de ce document");
                     }
                     $this->_helper->FlashMessenger->setNamespace('success')->addMessage($success);
                 } else {
