7eb7f42f4cb0c7016f59c863c3c544c042dc9c23
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-08-29 00:34:24 +0200

    fix autoloading order
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- public/const.php
+++ public/const.php
@@ -1,5 +1,10 @@
 <?php
 
+// Ensure autoloader is loaded
+if (!class_exists('Composer\Autoload\ClassLoader') && file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
 use Symfony\Component\Dotenv\Dotenv;
 
 function defineProtocol(): void
