66b9735e733f382d47c03f8166001a3fd14c21da
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-09-29 14:20:46 +0200

    fix duplicated authors
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- library/Episciences/Repositories/ARCHE/Hooks.php
+++ library/Episciences/Repositories/ARCHE/Hooks.php
@@ -169,29 +169,43 @@ class Episciences_Repositories_ARCHE_Hooks implements Episciences_Repositories_H
     private static function extractPersons($metadata, &$creatorsDc): array
     {
         $authors = [];
+        $seenNames = [];
 
         // Find creators first (primary authors)
         $creators = $metadata->xpath('//datacite:creators/datacite:creator');
         foreach ($creators as $creator) {
-            $person = self::processPerson($creator, 'creatorName', $creatorsDc);
+            $person = self::processPerson($creator, 'creatorName', $creatorsDc, $seenNames);
             if (!empty($person)) {
                 $authors[] = $person;
             }
         }
 
-        // Find contributors (additional contributors)
+        // Find contributors (additional contributors) - only include individual contributors, not institutions
         $contributors = $metadata->xpath('//datacite:contributors/datacite:contributor');
         foreach ($contributors as $contributor) {
-            $person = self::processPerson($contributor, 'contributorName', $creatorsDc);
-            if (!empty($person)) {
-                $authors[] = $person;
+            $contributorType = (string)$contributor['contributorType'];
+
+            // Only process individual contributors, skip institutional ones
+            $individualContributorTypes = [
+                'ContactPerson',
+                'DataCurator',
+                'Editor',
+                'ProjectLeader',
+                'Other'
+            ];
+
+            if (in_array($contributorType, $individualContributorTypes)) {
+                $person = self::processPerson($contributor, 'contributorName', $creatorsDc, $seenNames);
+                if (!empty($person)) {
+                    $authors[] = $person;
+                }
             }
         }
 
         return $authors;
     }
 
-    private static function processPerson($person, $nameField, &$creatorsDc): array
+    private static function processPerson($person, $nameField, &$creatorsDc, &$seenNames): array
     {
         $affiliations = [];
         $tmp = [];
@@ -202,6 +216,13 @@ class Episciences_Repositories_ARCHE_Hooks implements Episciences_Repositories_H
         }
 
         $name = $personName;
+
+        // Check for duplicates
+        if (in_array($name, $seenNames)) {
+            return [];
+        }
+
+        $seenNames[] = $name;
         $creatorsDc[] = $name;
 
         // Parse name assuming "First Last" format (not "Last, First")
