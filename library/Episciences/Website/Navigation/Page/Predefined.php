f3e07cb3921903d70336705631c43331d668a4a8
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-11-25 22:10:51 +0100

    - Replaced switch without default by if/else - reformatted code - added tests
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- library/Episciences/Website/Navigation/Page/Predefined.php
+++ library/Episciences/Website/Navigation/Page/Predefined.php
@@ -3,95 +3,22 @@
 class Episciences_Website_Navigation_Page_Predefined extends Episciences_Website_Navigation_Page
 {
     const PERMALIEN = 'permalien';
+    private static ?array $_cachedPermaliens = null;
     protected $_controller = 'page';
     protected $_multiple = false;
     protected string $_page = '';
 
-    private static ?array $_cachedPermaliens = null;
-
-
-    public function toArray(): array
+    /**
+     * Vérifie si un pageCode (permalien) correspond à une page prédéfinie
+     *
+     * @param string $pageCode Le permalien à vérifier
+     * @return bool True si le pageCode correspond à une page prédéfinie
+     * @throws ReflectionException
+     */
+    public static function isPredefinedPage(string $pageCode): bool
     {
-        $array = parent::toArray();
-        $array[self::PERMALIEN] = $this->getPermalien();
-        return $array;
-    }
-
-
-    public function getPermalien(): string
-    {
-        return $this->_permalien;
-    }
-
-
-    public function setPermalien($permalien): void
-    {
-        $this->_permalien = $permalien;
-    }
-
-
-    public function getAction(): string
-    {
-        return $this->getPermalien();
-    }
-
-
-    public function getForm($pageidx)
-    {
-        parent::getForm($pageidx);
-        if (!$this->_form->getElement(self::PERMALIEN)) {
-            $this->_form->addElement('hidden', self::PERMALIEN, [
-                'required' => true,
-                'value' => $this->getPermalien(),
-                'belongsTo' => 'pages_' . $pageidx,
-                'class' => 'permalien',
-            ]);
-        }
-
-        $this->_form->addElement('select', 'visibility',
-            ['label' => 'Visibilité de la page',
-                'belongsTo' => 'pages_' . $pageidx,
-                'onchange' => "setVisibility($pageidx, this)",
-                'multioptions' => ['Publique'], // Predefined pages are always public
-                'value' => 0 // 0 means public
-            ]);
-
-        $this->_form->getElement('labels')->setOptions(['class' => 'inputlangmulti permalien-src']);
-        return $this->_form;
-    }
-
-
-    public function setOptions($options = []): void
-    {
-        foreach ($options as $option => $value) {
-            $option = strtolower($option);
-
-            switch ($option) {
-                case self::PERMALIEN:
-                    $this->setPermalien($this->_permalien);
-                    break;
-                case 'page':
-                    $this->setPage($value);
-                    break;
-            }
-        }
-
-        parent::setOptions($options);
-    }
-
-
-    public function setPage($page): void
-    {
-        $this->_page = $page;
-    }
-
-    public function getSuppParams(): string
-    {
-        $res = '';
-        if ($this->_permalien != '') {
-            $res = serialize([self::PERMALIEN => $this->_permalien]);
-        }
-        return $res;
+        $permaliens = self::getAllPermaliens();
+        return in_array($pageCode, $permaliens, true);
     }
 
     /**
@@ -129,17 +56,79 @@ class Episciences_Website_Navigation_Page_Predefined extends Episciences_Website
         return self::$_cachedPermaliens;
     }
 
-    /**
-     * Vérifie si un pageCode (permalien) correspond à une page prédéfinie
-     *
-     * @param string $pageCode Le permalien à vérifier
-     * @return bool True si le pageCode correspond à une page prédéfinie
-     * @throws ReflectionException
-     */
-    public static function isPredefinedPage(string $pageCode): bool
+    public function toArray(): array
     {
-        $permaliens = self::getAllPermaliens();
-        return in_array($pageCode, $permaliens, true);
+        $array = parent::toArray();
+        $array[self::PERMALIEN] = $this->getPermalien();
+        return $array;
+    }
+
+    public function getPermalien(): string
+    {
+        return $this->_permalien;
+    }
+
+    public function getAction(): string
+    {
+        return $this->getPermalien();
+    }
+
+    public function getForm($pageidx)
+    {
+        parent::getForm($pageidx);
+        if (!$this->_form->getElement(self::PERMALIEN)) {
+            $this->_form->addElement('hidden', self::PERMALIEN, [
+                'required' => true,
+                'value' => $this->getPermalien(),
+                'belongsTo' => 'pages_' . $pageidx,
+                'class' => 'permalien',
+            ]);
+        }
+
+        $this->_form->addElement('select', 'visibility',
+            ['label' => 'Visibilité de la page',
+                'belongsTo' => 'pages_' . $pageidx,
+                'onchange' => "setVisibility($pageidx, this)",
+                'multioptions' => ['Publique'], // Predefined pages are always public
+                'value' => 0 // 0 means public
+            ]);
+
+        $this->_form->getElement('labels')->setOptions(['class' => 'inputlangmulti permalien-src']);
+        return $this->_form;
+    }
+
+    public function setOptions($options = []): void
+    {
+        foreach ($options as $option => $value) {
+            $option = strtolower($option);
+
+            if ($option == self::PERMALIEN) {
+                $this->setPermalien($this->_permalien);
+            } elseif ($option == 'page') {
+                $this->setPage($value);
+            }
+        }
+
+        parent::setOptions($options);
+    }
+
+    public function setPermalien($permalien): void
+    {
+        $this->_permalien = $permalien;
+    }
+
+    public function setPage($page): void
+    {
+        $this->_page = $page;
+    }
+
+    public function getSuppParams(): string
+    {
+        $res = '';
+        if ($this->_permalien != '') {
+            $res = serialize([self::PERMALIEN => $this->_permalien]);
+        }
+        return $res;
     }
 
 }
