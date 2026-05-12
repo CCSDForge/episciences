04d83133f629cbd89c438f5f747bcf95eaf3851a
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2026-05-12 14:07:50 +0200

    removed SPDX license translations.

--- application/languages/fr/non_spdx_licenses.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-        "NOASSERTION-HAL-Authorisation-1.0" => "Hal authorisation v1",
-        "NOASSERTION-HAL-Copyright" => "Copyright - Tous droits réservés",
-        "NOASSERTION-ARXIV-1.0-nonexclusive-distrib" => "arXiv.org - Licence non exclusive de distribution",
-        "NOASSERTION-ARXIV-assumed-1991-2003" => "arXiv.org - Licence non exclusive de distribution présumée"
-);
