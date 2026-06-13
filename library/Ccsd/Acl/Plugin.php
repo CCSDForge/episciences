32b30ee7cbe515a9a2896ad3b99fd145acbabcd8
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2026-06-13 10:46:20 +0200

    added obvious types
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- library/Ccsd/Acl/Plugin.php
+++ library/Ccsd/Acl/Plugin.php
@@ -18,15 +18,13 @@ class Ccsd_Acl_Plugin extends Zend_Controller_Plugin_Abstract
     /**
      * Action permettant la redirection de l'utilisateur lorsqu'il n'est pas
      * autorisé à accéder à la ressource
-     *
-     * @var unknown_type
      */
     const FAIL_AUTH_ACTION = 'error';
-    
+
     const PAGENOTFOUND_ACTION = 'pagenotfound';
 
     protected $_acl = null;
-    
+
     /**
      *
      * @see Zend_Controller_Plugin_Abstract::preDispatch()
@@ -37,7 +35,7 @@ class Ccsd_Acl_Plugin extends Zend_Controller_Plugin_Abstract
     	$this->_acl = $this->getAcl();
         // Récupération de l'id de la ressource (à modifier)
         $resource = $request->getControllerName() . '-' . $request->getActionName();
-        
+
         if (! $this->isAllowed($resource)) {
             $request->setControllerName(self::FAIL_AUTH_CONTROLLER);
             $request->setActionName(self::FAIL_AUTH_ACTION);
@@ -46,25 +44,20 @@ class Ccsd_Acl_Plugin extends Zend_Controller_Plugin_Abstract
         }
 
     }
-    
-    /**
-     * Méthode permettant de récupérer les ACL d'une application
-     * A redéfinir pour chaque application
-     * @return multitype:
-     */
-    public function getAcl()
+
+    public function getAcl(): array
     {
-    	return array();
+    	return [];
     }
-    
+
     /**
      * Méthode spécifique par application pour définir si une ressource peut être consultée par une personne
      * @param string $resource
      * @return boolean
      */
-    public function isAllowed($resource)
+    public function isAllowed($resource): bool
     {
         return true;
     }
-    
+
 }
